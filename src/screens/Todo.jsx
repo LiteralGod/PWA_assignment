35994e91cf30e613ec8a42337f809d67f18ac938
LiteralGod <tchri20@student.sdu.dk>
2022-11-18 00:15:27 +0100

    initial commit

---
+++ src/screens/Todo.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import './todo.css'
+
+
+function Todo() {
+    return (
+      <div className='todotitletext'>
+        <text>To-do list </text>
+      </div>
+    );
+  }
+  
+  export default Todo;
+  
